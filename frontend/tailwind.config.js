0636a25c17511a0b5fa0137efe7dfeb1b1fc3730
mahefarazonarison <rivozons@gmail.com>
2026-02-20 14:46:12 +0100

    refactor(qute): daisyUI et tailwind

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "../src/main/resources/templates/**/*.html"
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [require("daisyui")],
+    daisyui: {
+        themes: ["corporate"]
+    }
+}
